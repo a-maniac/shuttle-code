f5e7b9da165f21b430d151b1b3165bf2313cd456
amanjoshi <ajoshi373@gmail.com>
2024-07-06 22:48:59 +0530

    added comment

--- src/main/java/com/example/IntroToSpringBoot/IntroToSpringBootApplication.java
+++ src/main/java/com/example/IntroToSpringBoot/IntroToSpringBootApplication.java
@@ -25,7 +25,7 @@ public class IntroToSpringBootApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 //		apple.eatApple();
 //		apple2.eatApple();
-		//implement log rather than Sysout.
+		//implement log rather than Sysout
 		System.out.println(cakeBaker.bakeCake());
 	}
 
